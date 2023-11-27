7e86f53bad0166d1fd16fd22483606b41f77fd3d
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-11-26 20:34:00 -0600

    added shopping cart 2

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,11 +1,16 @@
 class CartController < ApplicationController
   def create
-    logger.debug("Adding #{params[:id]} to cart")
+    logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    session[:shopping_cart] << id
+    session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
+    car = Car.find(id)
+    flash[:notice] = "+ #{car.brand} #{car.model} added to cart..."
     redirect_to root_path
   end
-  def destroy;
-
+  def destroy
+    logger.debug("removing #{params[:id]} from the Cart.")
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    redirect_to root_path
   end
 end
