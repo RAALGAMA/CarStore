f82fd307cfda6659a58d55bd68f08ba66de87577
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-11-26 18:24:53 -0600

    added pagination

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,6 +4,13 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
+  def index
+    @cars = Car.page(params[:page]).per(10) # Muestra 10 carros por página
+    session[:visit_count] || = 0
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
+  end
+
   # GET /cars/search?keywords=user+search+terms
   def search
     wildcard_search = "%#{params[:search_term]}%"
