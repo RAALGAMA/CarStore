8c7253e8c83fcd7560f7933594c7a6b1bf9f85ed
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-11-30 16:56:12 -0600

    create table orderitems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_225140) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_225510) do
   create_table "abouts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -96,6 +96,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_225140) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "car_id", null: false
+    t.integer "quantity"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_order_items_on_car_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
   create_table "social_media", force: :cascade do |t|
     t.string "twitter"
     t.string "facebook"
@@ -127,5 +138,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_225140) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cars", "manufacturers"
+  add_foreign_key "order_items", "cars"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "social_media", "manufacturers"
 end
